0d57e7c7773e34100797b7b4d3775823ab8801d1
Sthvnbb <sabolanosb@hotmail.com>
2016-09-02 16:10:03 -0600

    Segunda carga de proyecto

--- ProyectoLlamadas/app/src/main/java/com/example/centauro/proyectollamadas/MainActivity.java
+++ ProyectoLlamadas/app/src/main/java/com/example/centauro/proyectollamadas/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.centauro.proyectollamadas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b_zero, b_one, b_two, b_three, b_four, b_five, b_six, b_seven, b_eight, b_nine, b_call, b_star, b_hashtag;
+    TextView screen;
+    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_one = (Button) findViewById(R.id.button_zero);
+        b_two = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+        b_zero = (Button) findViewById(R.id.button_zero);
+
+        b_zero = (Button) findViewById(R.id.button_zero);
     }
 }
