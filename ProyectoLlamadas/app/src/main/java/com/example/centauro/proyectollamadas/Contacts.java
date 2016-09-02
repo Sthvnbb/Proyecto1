c17502404eef63af8ac0c29093131926dbe732a9
camiavila08 <camiavilasalazar@gmail.com>
2016-09-02 15:37:31 -0600

    contactos

---
+++ ProyectoLlamadas/app/src/main/java/com/example/centauro/proyectollamadas/Contacts.java
@@ -0,0 +1,51 @@
+package com.example.centauro.proyectollamadas;
+
+import android.app.Activity;
+
+public class Contacts extends Activity {
+
+        private String nombre1 = "Gabriel";
+        private String nombre2 = "Carolina";
+        private String nombre3 = "David";
+
+        private String numero1 = "87516484";
+        private String numero2 = "88631158";
+        private String numero3 = "83851046";
+
+    public String Nombre1() {
+
+        return nombre1;
+
+    }
+
+    public String Numero1() {
+
+        return numero1;
+
+    }
+
+    public String Nombre2() {
+
+        return nombre2;
+
+    }
+
+    public String Numero2() {
+
+        return numero2;
+
+    }
+
+    public String Nombre3() {
+
+        return nombre3;
+
+    }
+
+    public String Numero3() {
+
+        return numero3;
+
+    }
+
+}
